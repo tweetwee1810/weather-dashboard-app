dab1cea4a6c15be766fd4560d5a2cac0095fb697
Mona Lisa <tweetwee1996@gmail.com>
2022-12-19 19:01:02 -0800

    updated function js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,13 @@ var city;
 var baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var search = document.querySelector(".form-control");
 var searchBtn = document.querySelector("#button-addon2");
-var location = document.querySelector(".weather-location");
+var weatherLocation = document.querySelector(".weather-location");
 var weatherIcon = document.querySelector("#icon");
 var tempt = document.querySelector("#temp");
 var humid = document.querySelector("#humid");
 var windy = document.querySelector("#wind");
 var currentWeather = document.querySelector(".current-city");
-var dateDisplay = moment().format("MMM Do YY");
+var dateDisplay = dayjs().format("MM/DD/YY");
 var previousSearch = [];
 
 //function to save the history search
@@ -18,11 +18,9 @@ function saveHistory(searchInput) {
     previousSearch.push(searchInput)
     localStorage.setItem('previousSearch', JSON.stringify(previousSearch))
 }
-//function to display history search
-function displayHistory (){
-    JSON.parse(localStorage.getItem('previousSearch', previousSearch));
-}
-
+// //function to display history search
+// function displayHistory (){
+//     JSON.parse(localStorage.getItem('previousSearch', previousSearch));
 //function for call api
 function getWeatherData(city) {
     fetch(baseURL + city + '&units=imperial&appid=' + APIkey)
@@ -30,13 +28,13 @@ function getWeatherData(city) {
             return response.json();
         })
         .then(function (data) {
-            
-            document.querySelector('#date').innerHTML = dateDisplay;
-            location.innerHTML = data.name;
+            // console.log(data)
+            weatherLocation.innerHTML = data.name;
+            document.querySelector('#current-location').innerHTML = dateDisplay;
             weatherIcon.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
             tempt.innerHTML = 'Temp: ' + data.main.temp + "°F";
             humid.innerHTML = 'Humidity: ' + data.main.humidity + "%";
-            windy.innerHTML = 'Wind: ' + data.wind.speed + "m/h";
+            windy.innerHTML = 'Wind: ' + data.wind.speed + "MPH";
 
 
             var lon = data.coord.lon;
@@ -48,40 +46,45 @@ function getWeatherData(city) {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log(data);
-                    document.querySelector('#date1').innerHTML = data.list[0].dt_txt;
-                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[0].weather[0].icon + '@2x.png';
-                    console.log(data)
-                    document.querySelector('#temp1').innerHTML = 'Temp:' + data.list[0].main.temp + '°F';
-                    document.querySelector('#wind1').innerHTML = 'Wind: ' + data.list[0].wind.speed + ' m/h';
-                    document.querySelector('#humid1').innerHTML = 'Humidity: ' + data.list[0].main.humidity + '%';
+                console.log(data)
+                // console.log(data.list[0])
+                //day 1
+            document.querySelector("#date1").innerHTML = dayjs().add(1, "day").format("MM/DD/YY");
+            console.log(data.list[0].weather[0].icon)
+            document.querySelector("#status-1").src = 'https://openweathermap.org/img/wn/' + data.list[0].weather[0].icon + '@2x.png';
+            document.querySelector("#temp-1").innerHTML = "Temp: " + data.list[0].main.temp + "°F";
+            document.querySelector("#humid-1").innerHTML = "Humidity: " + data.list[0].main.humidity + "%";
+            document.querySelector("#wind-1").innerHTML = "Wind: " + data.list[0].wind.speed + "MPH";
+            
+            //day 2 
+            document.querySelector("#date2").innerHTML = dayjs().add(2, "day").format("MM/DD/YY");
+            document.querySelector("#status-2").src = 'https://openweathermap.org/img/wn/' + data.list[7].weather[0].icon + '@2x.png';
+            document.querySelector("#temp-2").innerHTML = "Temp: " + data.list[7].main.temp + "°F";
+            document.querySelector("#humid-2").innerHTML = "Humidity: " + data.list[7].main.humidity + "%";
+            document.querySelector("#wind-2").innerHTML = "Wind: " + data.list[7].wind.speed + "MPH";
+            //day 3
+            document.querySelector("#date3").innerHTML = dayjs().add(3, "day").format("MM/DD/YY");
+            document.querySelector("#status-3").src = 'https://openweathermap.org/img/wn/' + data.list[14].weather[0].icon + '@2x.png';
+            document.querySelector("#temp-3").innerHTML = "Temp: " + data.list[14].main.temp + "°F";
+            document.querySelector("#humid-3").innerHTML = "Humidity: " + data.list[14].main.humidity + "%";
+            document.querySelector("#wind-3").innerHTML = "Wind: " + data.list[14].wind.speed + "MPH";
+            //day 4
+            document.querySelector("#date4").innerHTML = dayjs().add(4, "day").format("MM/DD/YY");
+            document.querySelector("#status-4").src = 'https://openweathermap.org/img/wn/' + data.list[21].weather[0].icon + '@2x.png';
+            document.querySelector("#temp-4").innerHTML = "Temp: " + data.list[21].main.temp + "°F";
+            document.querySelector("#humid-4").innerHTML = "Humidity: " + data.list[21].main.humidity + "%";
+            document.querySelector("#wind-4").innerHTML = "Wind: " + data.list[21].wind.speed + "MPH";
 
-                    document.querySelector('#date2').innerHTML = data.list[8].dt_txt;
-                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[1].weather[1].icon + '@2x.png';
-                    document.querySelector('#temp2').innerHTML = 'Temp: ' + data.list[8].main.temp + '°F';
-                    document.querySelector('#wind2').innerHTML = 'Wind: ' + data.list[8].wind.speed + ' m/h';
-                    document.querySelector('#humid2').innerHTML = 'Humidity: ' + data.list[8].main.humidity + '%';
-
-                    document.querySelector('#date3').innerHTML = data.list[16].dt_txt;
-                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[2].weather[2].icon + '@2x.png';
-                    document.querySelector('#temp3').innerHTML = 'Temp: ' + data.list[16].main.temp + '°F';
-                    document.querySelector('#wind3').innerHTML = 'Wind: ' + data.list[16].wind.speed + ' m/h';
-                    document.querySelector('#humid3').innerHTML = 'Humidity: ' + data.list[16].main.humidity + '%';
-
-                    document.querySelector('#date4').innerHTML = data.list[24].dt_txt;
-                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[3].weather[3].icon + '@2x.png';
-                    document.querySelector('#temp4').innerHTML = 'Temp: ' + data.list[24].main.temp + '°F';
-                    document.querySelector('#wind4').innerHTML = 'Wind: ' + data.list[24].wind.speed + ' m/h';
-                    document.querySelector('#humid4').innerHTML = 'Humidity: ' + data.list[24].main.humidity + '%';
-
-                    document.querySelector('#date5').innerHTML = data.list[32].dt_txt;
-                    //iconn.src = 'https://openweathermap.org/img/wn/' + data.list[4].weather[4].icon + '@2x.png';
-                    document.querySelector('#temp5').innerHTML = 'Temp: ' + data.list[32].main.temp + '°F';
-                    document.querySelector('#wind5').innerHTML = 'Wind: ' + data.list[32].wind.speed + ' m/h';
-                    document.querySelector('#humid5').innerHTML = 'Humidity: ' + data.list[32].main.humidity + '%';
-    })
-})
+            //day 5
+            document.querySelector("#date5").innerHTML = dayjs().add(5, "day").format("MM/DD/YY");
+            document.querySelector("#status-5").src = 'https://openweathermap.org/img/wn/' + data.list[28].weather[0].icon + '@2x.png';
+            document.querySelector("#temp-5").innerHTML = "Temp: " + data.list[28].main.temp + "°F";
+            document.querySelector("#humid-5").innerHTML = "Humidity: " + data.list[28].main.humidity + "%";
+            document.querySelector("#wind-5").innerHTML = "Wind: " + data.list[28].wind.speed + "MPH";
+                })
+        })
 }
+// getWeatherData("tacoma");
 //button 
 searchBtn.addEventListener('click', function (event) {
     event.preventDefault();
@@ -89,5 +92,6 @@ searchBtn.addEventListener('click', function (event) {
     getWeatherData(city);
     saveHistory();
 })
-//call function to display
-displayHistory()
+// //call function to display
+// displayHistory()
+
